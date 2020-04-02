fda318ab17a5948f538f4459c347a71ce502814e
DuremarDuremar <duremar98@yandex,ru>
2020-04-03 00:25:49 +0300

    03.04.20

---
+++ script.js
@@ -0,0 +1,14 @@
+function insert(num) {
+  document.form.textviev.value = document.form.textviev.value + num;
+}
+function clean() {
+  document.form.textviev.value = " ";
+}
+function back() {
+  let exp = document.form.textviev.value;
+  document.form.textviev.value = exp.substring(0, exp.length - 1);
+}
+function equal() {
+  let exp = document.form.textviev.value;
+  document.form.textviev.value = eval(exp);
+}
